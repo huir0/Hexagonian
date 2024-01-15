0bac31abd4408bbe312e8a0b48b55e598f685f8e
huir0 <keronruss@gmail.com>
2024-01-15 17:58:08 +0900

    remove unnecessary files

--- sfaclog/test/widget_test.dart
+++ sfaclog/test/widget_test.dart
@@ -14,7 +14,7 @@ import 'package:sfaclog/view/app.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const SFACLOGApp());
+    await tester.pumpWidget(const SFACApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
