a22a438c510412ea6a48c2d28faf1241f0084cc3
Ko-Eunseo <koyk0408@naver.com>
2024-01-23 18:26:46 +0900

    ios 변경사항 업로드

--- sfaclog/macos/Flutter/GeneratedPluginRegistrant.swift
+++ sfaclog/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,9 +9,11 @@ import url_launcher_macos
 import google_sign_in_ios
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-}
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+import shared_preferences_foundation
+}
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
