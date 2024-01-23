7f2969dab2402dc54a2a0fe00ecb5ee5f50c17e8
Ko-Eunseo <koyk0408@naver.com>
2024-01-23 18:29:05 +0900

    Feat: 카카오sdk runApp 호출 전 초기화 #5

--- sfaclog/lib/main.dart
+++ sfaclog/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:sfaclog/view/app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  KakaoSdk.init(
+    nativeAppKey: '7f9236613997b0e48dfd6c5e9cd4d694',
+    javaScriptAppKey: '2face5ee56e8d9b825ad75cd03364a80',
+  );
   runApp(const ProviderScope(child: SFACLOGApp()));
 }
