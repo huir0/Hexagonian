62cadb1d899bf5775837da8a2e35b031bf21c9dd
sanghyun8039 <chltkdgus10@naver.com>
2024-01-24 14:23:30 +0900

    Add sfaclogWidget

--- sfaclog/macos/Flutter/GeneratedPluginRegistrant.swift
+++ sfaclog/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import url_launcher_macos
+import google_sign_in_ios
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+}
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
 }
