7f6c581cddc86f10a1cef0738a4d64febd2ff1e5
Ko-Eunseo <koyk0408@naver.com>
2024-01-24 18:30:21 +0900

    Feat: kakao login feature(in progress) #5

--- sfaclog/lib/view/widgets/kakao_login_button.dart
+++ sfaclog/lib/view/widgets/kakao_login_button.dart
@@ -1,47 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:sfaclog/data/datasource/pocketbase_auth.dart';
+import 'package:sfaclog/viewmodel/auth/kakao_auth/kakao_login_notifier.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sfaclog/viewmodel/auth/kakao_auth/kakao_login_state.dart';
 
-class KakaoLoginButton extends StatelessWidget {
+class KakaoLoginButton extends ConsumerWidget {
   const KakaoLoginButton({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
-    void kakaoLogin() async {
-      if (await isKakaoTalkInstalled()) {
-        try {
-          await UserApi.instance.loginWithKakaoTalk();
-          print('카카오톡으로 로그인 성공');
-        } catch (error) {
-          print('카카오톡으로 로그인 실패 $error');
-
-          // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
-          // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
-          if (error is PlatformException && error.code == 'CANCELED') {
-            return;
-          }
-          // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
-          try {
-            await UserApi.instance.loginWithKakaoAccount();
-            print('카카오계정으로 로그인 성공');
-          } catch (error) {
-            print('카카오계정으로 로그인 실패 $error');
-          }
-        }
-      } else {
-        try {
-          await UserApi.instance.loginWithKakaoAccount();
-          print('카카오계정으로 로그인 성공');
-        } catch (error) {
-          print('카카오계정으로 로그인 실패 $error');
-        }
-      }
-    }
-
+  Widget build(BuildContext context, WidgetRef ref) {
+    User? userData;
+    print(userData);
     return GestureDetector(
-      onTap: kakaoLogin,
+      onTap: () async {
+        try {
+          ref.read(kakaoLoginProvider.notifier).kakaoLogin();
+
+          if (ref.read(kakaoLoginProvider).authStatus ==
+              SNSAuthStatus.success) {
+            userData = ref.read(kakaoLoginProvider).userInfo;
+            ref.read(pocketbaseAuthProvider).setUserDataPbFromKakao(userData!);
+          }
+        } catch (e) {
+          print('카카오로그인 실패: $e');
+        }
+      },
       child: SizedBox(
         width: 50,
         height: 50,
