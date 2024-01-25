65ac74b1affe0d441856ea66231366f5b4f0962a
seonbeom <rhtmdqja120@gmail.comn>
2024-01-25 14:10:30 +0900

    naverlogin

---
+++ sfaclog/lib/view/pages/login_page/naver_login/sample_screen.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_naver_login/flutter_naver_login.dart';
+import 'package:sfaclog/view/pages/login_page/naver_login/login_platform.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: sampleLogin(),
+    ),
+  );
+}
+
+class sampleLogin extends StatefulWidget {
+  const sampleLogin({super.key});
+
+  @override
+  State<sampleLogin> createState() => _sampleLoginState();
+}
+
+class _sampleLoginState extends State<sampleLogin> {
+  LoginPlatform _loginPlatform = LoginPlatform.none;
+
+  void signInWithNaver() async {
+    final NaverLoginResult result = await FlutterNaverLogin.logIn();
+
+    if (result.status == NaverLoginStatus.loggedIn) {
+      print('accessToken = ${result.accessToken}');
+      print('id = ${result.account.id}');
+      print('email = ${result.account.email}');
+      print('name = ${result.account.name}');
+
+      setState(() {
+        _loginPlatform = LoginPlatform.naver;
+      });
+    }
+  }
+
+  void signOut() async {
+    switch (_loginPlatform) {
+      case LoginPlatform.facebook:
+        break;
+      case LoginPlatform.google:
+        break;
+      case LoginPlatform.kakao:
+        break;
+      case LoginPlatform.naver:
+        await FlutterNaverLogin.logOut();
+        break;
+      case LoginPlatform.apple:
+        break;
+      case LoginPlatform.none:
+        break;
+    }
+    setState(() {
+      _loginPlatform = LoginPlatform.none;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _loginPlatform != LoginPlatform.none
+            ? _logoutButton()
+            : Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _loginButton(
+                    'naver_logo',
+                    signInWithNaver,
+                  ),
+                ],
+              ),
+      ),
+    );
+  }
+
+  Widget _loginButton(String path, VoidCallback onTap) {
+    return Card(
+      elevation: 5.0,
+      shape: const CircleBorder(),
+      clipBehavior: Clip.antiAlias,
+      child: Ink.image(
+        image: AssetImage('asset/image/$path.png'),
+        width: 60,
+        height: 60,
+        child: InkWell(
+          borderRadius: const BorderRadius.all(
+            Radius.circular(35.0),
+          ),
+          onTap: onTap,
+        ),
+      ),
+    );
+  }
+
+  Widget _logoutButton() {
+    return ElevatedButton(
+      onPressed: signOut,
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(
+          const Color(0xff0165E1),
+        ),
+      ),
+      child: const Text('로그아웃'),
+    );
+  }
+}
