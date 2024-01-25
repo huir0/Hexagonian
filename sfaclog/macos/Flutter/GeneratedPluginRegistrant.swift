ed7229c0b13a5002d825e5c818c759a320380f13
Ko-Eunseo <koyk0408@naver.com>
2024-01-25 17:38:56 +0900

    플러그인 관련 수정사항

--- sfaclog/macos/Flutter/GeneratedPluginRegistrant.swift
+++ sfaclog/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,7 +13,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 import shared_preferences_foundation
 }
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
