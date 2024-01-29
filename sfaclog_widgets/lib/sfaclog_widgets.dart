b528eeadb89866ce2bf4aed054c51048281ed172
Ko-Eunseo <koyk0408@naver.com>
2024-01-29 19:12:38 +0900

    Docs: 커스텀 위젯들 export #12

--- sfaclog_widgets/lib/sfaclog_widgets.dart
+++ sfaclog_widgets/lib/sfaclog_widgets.dart
@@ -6,3 +6,6 @@ export 'package:sfaclog_widgets/tooltips/bubble_tooltip.dart';
 export 'package:sfaclog_widgets/tags/sl_tag.dart';
 export 'package:sfaclog_widgets/textfields/sl_search_bar.dart';
 export 'package:sfaclog_widgets/textfields/sl_textfield.dart';
+export 'package:sfaclog_widgets/chips/chips.dart';
+export 'package:sfaclog_widgets/chips/sl_checkbox.dart';
+export 'package:sfaclog_widgets/tooltips/bubble_tooltip.dart';
