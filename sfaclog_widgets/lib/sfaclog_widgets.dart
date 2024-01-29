ae2bf0d6f1629a0cdbc57d68026bdc712b7975b6
Ko-Eunseo <koyk0408@naver.com>
2024-01-29 19:12:38 +0900

    Docs: 커스텀 위젯들 export #12

--- sfaclog_widgets/lib/sfaclog_widgets.dart
+++ sfaclog_widgets/lib/sfaclog_widgets.dart
@@ -1,23 +1,5 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 library sfaclog_widgets;
 
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-
-class SFACButton extends StatelessWidget {
-  final VoidCallback? onPressed;
-  final Widget? child;
-  SFACButton({
-    required this.onPressed,
-    required this.child,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-      onPressed: onPressed,
-      child: child,
-    );
-  }
-}
+export 'package:sfaclog_widgets/chips/chips.dart';
+export 'package:sfaclog_widgets/chips/sl_checkbox.dart';
+export 'package:sfaclog_widgets/tooltips/bubble_tooltip.dart';
