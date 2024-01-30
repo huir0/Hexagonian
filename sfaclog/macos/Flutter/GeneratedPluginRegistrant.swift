39627b57932f65bfa57749e977c6ce1815be84ab
Ko-Eunseo <koyk0408@naver.com>
2024-01-30 17:16:15 +0900

    url 런처 플러그인 관련 코드 추가

--- sfaclog/macos/Flutter/GeneratedPluginRegistrant.swift
+++ sfaclog/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,12 @@ import Foundation
 
 import url_launcher_macos
 import google_sign_in_ios
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
