94aa0da8d9ddf40e31a290f078b868611364531a
sanghyun8039 <chltkdgus10@naver.com>
2024-01-31 14:05:21 +0900

    add slbottomSheet

---
+++ sfaclog_widgets/lib/bottomsheets/sl_bottom_sheets.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:sfaclog_widgets/util/common.dart';
+
+class SLSheet {
+  static void bottomSheet(
+      {required BuildContext context,
+      required Widget child,
+      required double height,
+      required double width}) {
+    showModalBottomSheet(
+        isScrollControlled: true,
+        useSafeArea: true,
+        context: context,
+        builder: (context) {
+          return Container(
+            decoration: BoxDecoration(
+                color: SLColor.neutral.shade90,
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20))),
+            width: width,
+            height: height,
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 14,
+                ),
+                Container(
+                  height: 5,
+                  width: 36,
+                  decoration: BoxDecoration(
+                      color: SLColor.neutral.shade60,
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(100))),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                child
+              ],
+            ),
+          );
+        });
+  }
+}
