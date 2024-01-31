15145eedb0e622a20d7d4a1de731ced8be4e479e
Ko-Eunseo <koyk0408@naver.com>
2024-01-31 15:27:28 +0900

    pull develop

--- sfaclog/macos/Flutter/GeneratedPluginRegistrant.swift
+++ sfaclog/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -2,14 +2,11 @@
 //  Generated file. Do not edit.
 //
 
-#import FlutterMacOS
-#import Foundation
+import FlutterMacOS
+import Foundation
 
-#import google_sign_in_ios
-#import url_launcher_macos
+import google_sign_in_ios
 
-void RegisterGeneratedPlugins(FlutterPluginRegistry* registry) {
-  [FLTGoogleSignInPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTGoogleSignInPlugin"]];
-  [UrlLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"UrlLauncherPlugin"]];
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
 }
-
