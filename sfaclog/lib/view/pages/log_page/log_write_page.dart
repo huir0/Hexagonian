2962ba26ff4e41e58bbd3fdf60d95d7851d27bae
sanghyun8039 <chltkdgus10@naver.com>
2024-02-01 09:09:49 +0900

    add markdown

--- sfaclog/lib/view/pages/log_page/log_write_page.dart
+++ sfaclog/lib/view/pages/log_page/log_write_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:markdown_toolbar/markdown_toolbar.dart';
 
 class LogWritePage extends StatefulWidget {
   const LogWritePage({super.key});
@@ -8,6 +11,24 @@ class LogWritePage extends StatefulWidget {
 }
 
 class _LogWritePageState extends State<LogWritePage> {
+  final TextEditingController _controller =
+      TextEditingController(); // Declare the TextEditingController
+  late final FocusNode _focusNode;
+  @override
+  void initState() {
+    _controller
+        .addListener(() => setState(() {})); // Update the text when typing
+    _focusNode = FocusNode(); // Assign a FocusNode
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose(); // Dispose the TextEditingController
+    _focusNode.dispose(); // Dispose the FocusNode
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,8 +37,26 @@ class _LogWritePageState extends State<LogWritePage> {
         centerTitle: true,
         actions: [TextButton(onPressed: () {}, child: const Text('완료'))],
       ),
-      body: const Column(
-        children: [],
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+                child: Column(
+              children: [
+                TextField(
+                  controller: _controller, // Add the _controller
+                  focusNode: _focusNode, // Add the _focusNode
+                ),
+              ],
+            )),
+          ),
+          MarkdownToolbar(
+            useIncludedTextField:
+                false, // Because we want to use our own, set useIncludedTextField to false
+            controller: _controller, // Add the _controller
+            focusNode: _focusNode, // Add the _focusNode
+          ),
+        ],
       ),
     );
   }
