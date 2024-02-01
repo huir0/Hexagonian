481822626755b31edaf3fa08036fb6f6afd308e9
Ko-Eunseo <koyk0408@naver.com>
2024-02-01 19:05:25 +0900

    UI: 온보딩(가입)페이지 구현중 #11

--- sfaclog/lib/view/pages/join_page/join_page.dart
+++ sfaclog/lib/view/pages/join_page/join_page.dart
@@ -1,108 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:sfaclog/view/widgets/join_page_widgets/name_mail_section.dart';
+import 'package:sfaclog/view/widgets/join_page_widgets/profile_section.dart';
+import 'package:sfaclog/view/widgets/join_page_widgets/propose_section.dart';
+import 'package:sfaclog/view/widgets/join_page_widgets/pw_confirm_section.dart';
+import 'package:sfaclog/view/widgets/join_page_widgets/skill_section.dart';
+import 'package:sfaclog_widgets/util/common.dart';
 
 class JoinPage extends StatelessWidget {
   const JoinPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        padding: const EdgeInsets.all(24),
-        children: [
-          const Text('본인인증'),
-          const SizedBox(height: 20),
-          const TextFieldWidget(
-            type: TextFieldWidgetType.name,
-          ),
-          const SizedBox(height: 20),
-          const TextFieldWidget(
-            type: TextFieldWidgetType.email,
-          ),
-          const TextFieldWidget(type: TextFieldWidgetType.emailConfirm),
-          const SizedBox(height: 20),
-          const TextFieldWidget(
-            type: TextFieldWidgetType.password,
-          ),
-          const SizedBox(height: 20),
-          const TextFieldWidget(
-            type: TextFieldWidgetType.passwordConfirm,
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('다음'),
-          ),
-        ],
+    //@todo: riverpod으로 관리
+    int curIndex = 0;
+
+    final GlobalKey<FormState> nameMailFormKey = GlobalKey<FormState>();
+    final GlobalKey<FormState> pwConfirmFormKey = GlobalKey<FormState>();
+    final GlobalKey<FormState> profileFormKey = GlobalKey<FormState>();
+    final GlobalKey<FormState> skillFormKey = GlobalKey<FormState>();
+    final GlobalKey<FormState> proposeFormKey = GlobalKey<FormState>();
+
+    List<Widget> joinSections = [
+      NameMailSection(
+        formKey: nameMailFormKey,
       ),
-    );
-  }
-}
-
-enum TextFieldWidgetType {
-  name,
-  email,
-  emailConfirm,
-  password,
-  passwordConfirm,
-}
-
-class TextFieldWidget extends StatelessWidget {
-  const TextFieldWidget({
-    super.key,
-    this.controller,
-    required this.type,
-  });
-
-  final TextFieldWidgetType type;
-  final TextEditingController? controller;
-
-  String getLabel() {
-    switch (type) {
-      case TextFieldWidgetType.name:
-        return '이름';
-      case TextFieldWidgetType.email:
-        return '이메일 인증';
-      case TextFieldWidgetType.password:
-        return '비밀번호';
-      case TextFieldWidgetType.passwordConfirm:
-        return '비밀번호 확인';
-      default:
-        return '';
-    }
-  }
-
-  String getHintText() {
-    switch (type) {
-      case TextFieldWidgetType.name:
-        return '이름을 입력해 주세요.';
-      case TextFieldWidgetType.email:
-        return '이메일을 입력해 주세요.';
-      case TextFieldWidgetType.emailConfirm:
-        return '이메일 주소를 입력해 주세요.';
-      case TextFieldWidgetType.password:
-        return '비밀번호를 입력해 주세요.';
-      case TextFieldWidgetType.passwordConfirm:
-        return '비밀번호를 다시 입력해 주세요.';
-      default:
-        return '';
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    String label = getLabel();
-
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        label == '' ? Container() : Text(label),
-        TextField(
-          controller: controller,
-          decoration: InputDecoration(
-            hintText: getHintText(),
-            contentPadding: const EdgeInsets.symmetric(vertical: 11),
+      PwConfirmSection(
+        formKey: pwConfirmFormKey,
+      ),
+      ProfileSection(
+        formKey: profileFormKey,
+      ),
+      const SkillSection(
+          // formKey: skillFormKey,
           ),
+      const ProposeSection(
+          // formKey: proposeFormKey,
+          ),
+    ];
+
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
         ),
-      ],
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(height: 52),
+            LinearProgressIndicator(
+              minHeight: 2,
+              value: (curIndex + 1) / joinSections.length,
+              color: SLColor.primary,
+              backgroundColor: SLColor.neutral.shade80,
+            ),
+            const SizedBox(height: 34),
+            IndexedStack(
+              index: curIndex,
+              children: joinSections,
+            ),
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () {
+                if (curIndex < joinSections.length - 1) {
+                  curIndex++;
+                } else {
+                  curIndex = 0;
+                }
+                print(curIndex);
+              },
+              child: curIndex == joinSections.length - 1
+                  ? const Text('스팩로그 시작하기')
+                  : const Text('다음'),
+            ),
+            const SizedBox(height: 27),
+          ],
+        ),
+      ),
     );
   }
 }
