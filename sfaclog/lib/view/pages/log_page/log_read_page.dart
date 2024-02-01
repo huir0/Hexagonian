830a4fcfe60e7f5fe9e6dab0e420e77d9d4c5e08
sanghyun8039 <chltkdgus10@naver.com>
2024-02-01 09:09:49 +0900

    add markdown

--- sfaclog/lib/view/pages/log_page/log_read_page.dart
+++ sfaclog/lib/view/pages/log_page/log_read_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:go_router/go_router.dart';
 import 'package:sfaclog/common.dart';
 import 'package:sfaclog/view/widgets/log_read_page_widgets/log_read_appbar_widget.dart';
@@ -10,6 +11,11 @@ class LogReadPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var markDownData = ''' # 제목 1 
+## 제목 2
+### 제목 3
+#### 제목 4
+    ''';
     return SafeArea(
         child: Scaffold(
       appBar: const LogReadAppBarWidget(),
@@ -25,6 +31,18 @@ class LogReadPage extends StatelessWidget {
                     height: 1,
                     color: SLColor.neutral.shade80,
                   ),
+                  SizedBox(
+                      height: 400,
+                      child: Markdown(
+                        data: markDownData,
+                        styleSheet: MarkdownStyleSheet(
+                          h1: const TextStyle(
+                              fontSize: 24,
+                              color: Colors.red,
+                              fontWeight: FontWeight.w700),
+                          h2: const TextStyle(color: Colors.pink, fontSize: 30),
+                        ),
+                      ))
                 ],
               ),
             ),
