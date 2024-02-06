610e231f08e0d209e677c2b7bbf3851e3be76386
Ko-Eunseo <koyk0408@naver.com>
2024-02-06 23:05:01 +0900

    Refactor: AppWrapper, AppbarWidget 메인페이지에서 UI가 다르게 나오도록 수정 #44

--- sfaclog/lib/view/widgets/appbar_widget.dart
+++ sfaclog/lib/view/widgets/appbar_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:sfaclog/common.dart';
 
 class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
@@ -7,10 +8,41 @@ class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
     super.key,
     required this.pageLabel,
   });
+
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
   @override
   Widget build(BuildContext context) {
+    if (pageLabel == '홈') {
+      return AppBar(
+        automaticallyImplyLeading: false,
+        scrolledUnderElevation: 0,
+        title: Row(
+          children: [
+            SvgPicture.asset(
+              'assets/logo/sfaclog_main.svg',
+              width: 100,
+            ),
+            const SizedBox(width: 8),
+            SvgPicture.asset(
+              'assets/logo/sfacfolio.svg',
+              width: 90,
+            ),
+          ],
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        actions: [
+          IconButton(
+            onPressed: () {
+              print('Noti_Clicked');
+            },
+            icon: SvgPicture.asset('assets/icons/bell.svg'),
+          ),
+        ],
+      );
+    }
+
     return AppBar(
       automaticallyImplyLeading: false,
       scrolledUnderElevation: 0,
