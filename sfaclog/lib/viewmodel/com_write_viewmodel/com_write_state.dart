bd38cc49972c377ca80d2a7e67319813199c03ef
seonbeom <rhtmdqja120@gmail.comn>
2024-02-07 16:06:29 +0900

    write

---
+++ sfaclog/lib/viewmodel/com_write_viewmodel/com_write_state.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ComwriteState extends StatelessWidget {
+  const ComwriteState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
