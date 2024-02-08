4eb5f2e1f1b3d101e5a0028acd619977d1f7e142
Ko-Eunseo <85021536+Ko-Eunseo@users.noreply.github.com>
2024-02-08 14:25:50 +0900

    Revert "[Feat] 가입, 온보딩, 유저정보 세팅 기능"

--- sfaclog/android/app/src/main/kotlin/com/example/sfaclog/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sfaclog
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- sfaclog/lib/util/join_sections.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:sfaclog/view/widgets/join_page_widgets/name_mail_section.dart';
-import 'package:sfaclog/view/widgets/join_page_widgets/pw_confirm_section.dart';
-
-List<Widget> joinSections = [
-  const NameMailSection(),
-  const PwConfirmSection(),
-];
