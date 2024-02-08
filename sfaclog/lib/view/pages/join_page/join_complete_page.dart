c412a84827e0fc874d9aadabeed38f47ba567a9f
Ko-Eunseo <koyk0408@naver.com>
2024-02-08 12:24:40 +0900

    Feat: 회원가입 완료페이지 #62

--- sfaclog/lib/view/pages/join_page/join_complete_page.dart
+++ sfaclog/lib/view/pages/join_page/join_complete_page.dart
@@ -1,30 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:sfaclog/common.dart';
+import 'package:sfaclog/viewmodel/auth/onboarding_notifier.dart';
+import 'package:sfaclog/viewmodel/auth/onboarding_state.dart';
+import 'package:sfaclog_widgets/buttons/sl_button.dart';
 
-class JoinCompletePage extends StatelessWidget {
+class JoinCompletePage extends ConsumerWidget {
   const JoinCompletePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final onboardingNotifier = ref.read(onboardingProvider.notifier);
+
     const description = '회원가입이 정상적으로 \n완료되었습니다 :)';
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            Image.asset(
-              'assets/images/signup_complete.png',
+      body: Column(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/signup_complete.png',
+                ),
+                const SizedBox(height: 6),
+                Text(
+                  description,
+                  style: SLTextStyle(
+                    style: SLStyle.Text_L_Bold,
+                    color: SLColor.neutral.shade10,
+                  ).textStyle,
+                ),
+              ],
             ),
-            Text(
-              description,
-              style: SLTextStyle(
-                style: SLStyle.Text_L_Bold,
-                color: SLColor.neutral.shade10,
-              ).textStyle,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(24),
+            child: SLButton(
+              text: '다음',
+              isActive: true,
+              onTap: () {
+                onboardingNotifier.moveSpecifieSection(
+                  section: OnboardingStatus.section3,
+                );
+                context.pop();
+              },
             ),
-          ],
-        ),
+          ),
+        ],
       ),
-      // @todo redirect button
     );
   }
 }
