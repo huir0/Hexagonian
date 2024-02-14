53de776c271ff4b1ea13e8447f9e50011e2c9261
Ko-Eunseo <koyk0408@naver.com>
2024-02-14 10:22:43 +0900

    UI: 회원가입, 온보딩 레이아웃 수정

--- sfaclog/lib/view/pages/join_page/join_page.dart
+++ sfaclog/lib/view/pages/join_page/join_page.dart
@@ -14,31 +14,43 @@ class JoinPage extends ConsumerWidget {
 
     int curIndex = onboardingState.currentPage;
 
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 24,
-        ),
-        child: ListView(
-          children: [
-            const SizedBox(height: 52),
-            LinearProgressIndicator(
-              minHeight: 2,
-              value: (curIndex + 1) / onboardingState.totalPage,
-              color: SLColor.primary,
-              backgroundColor: SLColor.neutral.shade80,
+    return PopScope(
+      onPopInvoked: (didPop) async => false,
+      child: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Scaffold(
+          resizeToAvoidBottomInset: true,
+          body: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 24,
+              ),
+              child: ListView(
+                children: [
+                  const SizedBox(height: 52),
+                  LinearProgressIndicator(
+                    minHeight: 2,
+                    value: (curIndex + 1) / onboardingState.totalPage,
+                    color: SLColor.primary,
+                    backgroundColor: SLColor.neutral.shade80,
+                  ),
+                  const SizedBox(height: 34),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height - 130,
+                    child: IndexedStack(
+                      key: ValueKey<int>(onboardingState.currentPage),
+                      index: onboardingState.currentPage,
+                      children: [
+                        ...joinSections,
+                        ...onboardingSections,
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-            const SizedBox(height: 34),
-            IndexedStack(
-              key: ValueKey<int>(onboardingState.currentPage),
-              index: onboardingState.currentPage,
-              children: [
-                ...joinSections,
-                ...onboardingSections,
-              ],
-            ),
-          ],
+          ),
         ),
       ),
     );
--- sfaclog/lib/view/widgets/onboarding_page_widgets/profile_section.dart
+++ sfaclog/lib/view/widgets/onboarding_page_widgets/profile_section.dart
@@ -66,82 +66,82 @@ class ProfileSectionState extends ConsumerState<ProfileSection> {
       }
     }
 
-    print('프로필섹션: ${onboardingState.userInfo}');
-
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Form(
-          key: formKey,
-          onChanged: () {
-            if (formKey.currentState!.validate()) {
-              onboardingNotifier.setButtonEnabled(true);
-            } else {
-              onboardingNotifier.setButtonEnabled(false);
-            }
-          },
-          child: Column(
-            children: [
-              TitleWithCount(
-                  title: title,
-                  curIdx: onboardingState.currentPage + 1,
-                  total: onboardingState.totalPage),
-              const SizedBox(height: 8),
-              Text(
-                description,
-                style: SLTextStyle.Text_M_Regular?.copyWith(
-                  color: SLColor.neutral.shade30,
+        Expanded(
+          child: Form(
+            key: formKey,
+            onChanged: () {
+              if (formKey.currentState!.validate()) {
+                onboardingNotifier.setButtonEnabled(true);
+              } else {
+                onboardingNotifier.setButtonEnabled(false);
+              }
+            },
+            child: Column(
+              children: [
+                TitleWithCount(
+                    title: title,
+                    curIdx: onboardingState.currentPage + 1,
+                    total: onboardingState.totalPage),
+                const SizedBox(height: 8),
+                Text(
+                  description,
+                  style: SLTextStyle.Text_M_Regular?.copyWith(
+                    color: SLColor.neutral.shade30,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 57),
-              Align(
-                alignment: Alignment.center,
-                child: Stack(
-                  children: [
-                    profileImage(),
-                    Positioned(
-                      bottom: 0,
-                      right: 0,
-                      child: SLCircleIconButton(
-                        onTap: () {
-                          context.push('/profile');
-                        },
-                        width: editbtnSize,
-                        height: editbtnSize,
-                        icon: const Icon(
-                          Icons.add,
+                const SizedBox(height: 57),
+                Align(
+                  alignment: Alignment.center,
+                  child: Stack(
+                    children: [
+                      profileImage(),
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: SLCircleIconButton(
+                          onTap: () {
+                            context.push('/profile');
+                          },
+                          width: editbtnSize,
+                          height: editbtnSize,
+                          icon: const Icon(
+                            Icons.add,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(height: 50),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                    horizontal: inputPadding - wrapperPadding),
-                child: SLInput(
-                  maxLength: 10,
-                  fillColor: Colors.transparent,
-                  controller: controller,
-                  borderType: SLInputBorderType.underline,
-                  hintText: '닉네임을 입력해주세요',
-                  hintStyle: SLTextStyle.Text_L_Regular?.copyWith(
-                      color: SLColor.neutral.shade50),
-                  isCenteredText: true,
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  validator: (value) {
-                    if (value == null || value.trim().isEmpty) {
-                      return '닉네임을 입력해주세요.';
-                    }
-                    return null;
-                  },
-                  onSaved: (newValue) {
-                    nickname = newValue;
-                  },
+                const SizedBox(height: 50),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: inputPadding - wrapperPadding),
+                  child: SLInput(
+                    maxLength: 10,
+                    fillColor: Colors.transparent,
+                    controller: controller,
+                    borderType: SLInputBorderType.underline,
+                    hintText: '닉네임을 입력해주세요',
+                    hintStyle: SLTextStyle.Text_L_Regular?.copyWith(
+                        color: SLColor.neutral.shade50),
+                    isCenteredText: true,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: (value) {
+                      if (value == null || value.trim().isEmpty) {
+                        return '닉네임을 입력해주세요.';
+                      }
+                      return null;
+                    },
+                    onSaved: (newValue) {
+                      nickname = newValue;
+                    },
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
         SLButton(
