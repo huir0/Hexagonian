ba1f182ce669dc122ebe911df0acf2b150ba3b6b
sanghyun8039 <chltkdgus10@naver.com>
2024-02-15 00:09:45 +0900

    Update log_setting_page.dart

--- sfaclog/lib/view/pages/log_page/log_setting_page.dart
+++ sfaclog/lib/view/pages/log_page/log_setting_page.dart
@@ -79,10 +79,7 @@ class _LogSettingPageState extends ConsumerState<LogSettingPage> {
           TextButton(
               onPressed: () async {
                 List<String> imgUrlList = [];
-
-                /// 썸네일을 미리 저장하는 이유
-                /// 이미지 저장시 URL 을 Return 받기 위해
-                /// URL을 Body 다시 넣음으로써 Read시 호출 할 수 있음
+                // 썸네일을 미리 저장하여 Tag ID를 생성
                 var tagId = await remoteDataSource.uploadThumbNail(
                   'log',
                   imgPath,
@@ -96,7 +93,7 @@ class _LogSettingPageState extends ConsumerState<LogSettingPage> {
                   List<dynamic> body = jsonDecode(state.logModel!.body);
                   int imgUrlIndex = 0;
                   for (var item in body) {
-                    //기존 localPath로 되어있던 것들을 URL로 변경
+                    // 기존 localPath로 되어있던 것들을 URL로 변경
                     if (item['insert'] is Map<String, dynamic> &&
                         item['insert']['_type'] == 'image') {
                       // _type이 image인 항목의 source를 새로운 값으로 업데이트합니다.
@@ -109,13 +106,13 @@ class _LogSettingPageState extends ConsumerState<LogSettingPage> {
                   var newLogData =
                       state.logModel!.copyWith(body: convertedBody);
                   ref.read(logwriteProvider.notifier).setLog(newLogData);
-
+                  // 생성된 Tag ID에 변경된 Body Update
                   await remoteDataSource
                       .uploadLog('log', newLogData, tagId)
                       .then((value) {
                     context.push('/log/write/upload/$tagId');
                   });
-                  //이미지가 없다면
+                  // 이미지가 없다면
                 } else {
                   await remoteDataSource
                       .uploadLog('log', state.logModel!, tagId)
